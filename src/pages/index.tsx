e99ed0f81c14c3f15878b646418a2eb1f7fec180
Pavel <httpspal@protonmail.com>
2021-11-26 23:56:37 -0500

    TailwindCSS set up with typescript.

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+	return (
+		<div className="">
+			<Head>
+				<title>E-Commerce | Home</title>
+				<meta
+					name="description"
+					content="Full Stack website made with Next.js and AWS Amplify"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main></main>
+		</div>
+	);
+};
+
+export default Home;
